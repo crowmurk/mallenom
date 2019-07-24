7bb5e30699012f6ab90807801670e0427eced54d
crowmurk <crowmurk@gmail.com>
2019-07-24 22:26:47 +0300

    refractor: DeleteMessage mixins

--- mallenom/core/views.py
+++ mallenom/core/views.py
@@ -1,6 +1,8 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.db.models import ProtectedError
+from django.utils.translation import ugettext_lazy as _
 
 # Create your views here.
 
@@ -8,6 +10,7 @@ class ActionTableDeleteMixin:
     action_table_model = None
     action_table_button = "action-table-button"
     action_table_success_message = None
+    action_table_error_message = None
     action_table_multitables = None
 
     def post(self, request, *args, **kwargs):
@@ -17,6 +20,7 @@ class ActionTableDeleteMixin:
                     'model': self.action_table_model,
                     'button': self.action_table_button,
                     'success_message': self.action_table_success_message,
+                    'error_message': self.action_table_error_message,
                 }
             ]
         elif self.action_table_multitables:
@@ -41,23 +45,55 @@ class ActionTableDeleteMixin:
         for action_table in self.action_table_multitables:
             model = action_table['model']
             button = action_table['button']
+            error_message = action_table['error_message']
             success_message = action_table['success_message']
+
+            if not success_message:
+                success_message = _("{name} were deleted successfuly").format(
+                    name=model._meta.verbose_name_plural
+                )
+
+            if not error_message:
+                error_message = _("Cannot delete {name} because of"
+                                  " foreign references").format(
+                                      name=model._meta.verbose_name_plural
+                                  )
 
             button = request.POST.get(button)
             pks = request.POST.getlist(button)
             if pks:
                 selected_objects = model.objects.filter(pk__in=pks)
-                selected_objects.delete()
-                if success_message:
-                    messages.success(self.request, success_message)
+                try:
+                    selected_objects.delete()
+                    messages.success(request, success_message)
+                except ProtectedError:
+                    messages.error(request, error_message)
 
         return HttpResponseRedirect(request.path)
 
 
-class SuccessDeleteMessageMixin:
+class DeleteMessageMixin:
     success_message = None
+    error_message = None
 
     def delete(self, request, *args, **kwargs):
-        if self.success_message:
-            messages.success(self.request, self.success_message)
-        return super().delete(request, *args, **kwargs)
+        try:
+            result = super().delete(request, *args, **kwargs)
+            if self.success_message:
+                success_message = self.success_message
+            else:
+                success_message = _("{name} was deleted successfuly").format(
+                    name=self.model._meta.verbose_name
+                )
+            messages.success(self.request, success_message)
+        except ProtectedError:
+            if self.error_message:
+                error_message = self.error_message
+            else:
+                error_message = _("Cannot delete {name} because of"
+                                  " foreign references").format(
+                                      name=self.model._meta.verbose_name
+                                  )
+            messages.error(self.request, error_message)
+            return HttpResponseRedirect(self.object.get_absolute_url())
+        return result
