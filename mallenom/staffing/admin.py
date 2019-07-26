441bc6c61ac3a10588fa1505c9ff157fc7315e60
crowmurk <crowmurk@gmail.com>
2019-07-26 07:09:43 +0300

    feat: Add staffing in admin

--- mallenom/staffing/admin.py
+++ mallenom/staffing/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from .models import Department, Position, Staffing
+
 # Register your models here.
+
+class StaffingInline(admin.StackedInline):
+    model = Staffing
+    can_delete = True
+    extra = 0
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    inlines = (StaffingInline, )
+    search_fields = ('name', )
+
+
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+
+
+class StaffingAdmin(admin.ModelAdmin):
+    search_fields = (
+        'department__name',
+        'position__name',
+        'count',
+    )
+
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Staffing, StaffingAdmin)
