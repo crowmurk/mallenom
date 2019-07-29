50d52887fbacd1b2d898edca851f6e8ebcfd2bf6
crowmurk <crowmurk@gmail.com>
2019-07-29 06:24:20 +0300

    feat: Add employee in admin

--- mallenom/employee/admin.py
+++ mallenom/employee/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import Employee, Employment
+
 # Register your models here.
+
+class EmploymentInline(admin.StackedInline):
+    model = Employment
+    can_delete = True
+    extra = 0
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    inlines = (EmploymentInline, )
+    search_fields = ('full_name', )
+
+
+class EmploymentAdmin(admin.ModelAdmin):
+    search_fields = (
+        'number',
+    )
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Employment, EmploymentAdmin)
