e153cb12bb8a284965097013d374aafab8cbebcb
crowmurk <crowmurk@gmail.com>
2019-08-10 12:54:44 +0300

    feat: Add max hours check in assignments

--- mallenom/schedule/forms.py
+++ mallenom/schedule/forms.py
@@ -1,8 +1,10 @@
 import datetime
 
+from django.db import models
 from django import forms
-from django.forms import inlineformset_factory
 from django.utils.translation import gettext_lazy as _
+
+from workcal.models import Day
 
 from .models import Project, Assignment, ProjectAssignment, Absence
 
@@ -14,6 +16,12 @@ class ProjectForm(forms.ModelForm):
 
 
 class AssignmentForm(forms.ModelForm):
+    check_hours = forms.BooleanField(
+        required=False,
+        initial=True,
+        label=_('Check hours'),
+    )
+
     class Meta:
         model = Assignment
         exclude = ('projects', )
@@ -40,7 +48,6 @@ class AssignmentForm(forms.ModelForm):
                     },
                 ),
             )
-
         elif delta != datetime.timedelta(days=6):
             self.add_error(
                 'end',
@@ -56,9 +63,68 @@ class AssignmentForm(forms.ModelForm):
 
 
 class ProjectAssignmentForm(forms.ModelForm):
+    check_hours = forms.BooleanField(
+        required=False,
+        initial=True,
+        label=_('Check hours'),
+    )
+
     class Meta:
         model = ProjectAssignment
         fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if any(self.errors):
+            return cleaned_data
+
+        if not cleaned_data.get('check_hours'):
+            return cleaned_data
+
+        hours = cleaned_data['hours']
+        assignment = cleaned_data['assignment']
+
+        work_hours_max = Day.objects.get_work_hours_count(
+            assignment.start,
+            assignment.end,
+        )
+
+        hours_assigned = self._meta.model.objects.filter(
+            assignment=assignment,
+        )
+        if self.instance.pk:
+            hours_assigned = hours_assigned.exclude(
+                pk=self.instance.pk
+            )
+        hours_assigned = hours_assigned.aggregate(
+            hours=models.functions.Coalesce(
+                models.Sum('hours'), 0
+            ),
+        )['hours']
+
+        hours_available = work_hours_max - hours_assigned
+
+        if hours > hours_available:
+            self.add_error(
+                'hours',
+                forms.ValidationError(
+                    _("This value must be less than or"
+                      " equal to %(hours)s"),
+                    code='invalid',
+                    params={
+                        'hours': hours_available,
+                    },
+                ),
+            )
+        return cleaned_data
+
+
+class ProjectAssignmentFormsetForm(ProjectAssignmentForm):
+    check_hours = None
+
+    class Meta(ProjectAssignmentForm.Meta):
+        pass
 
 
 class AbsenceForm(forms.ModelForm):
@@ -89,10 +155,56 @@ class AbsenceForm(forms.ModelForm):
         return cleaned_data
 
 
-ProjectAssignmentFormSet = inlineformset_factory(
+class BaseProjectAssignmentFormSet(forms.BaseInlineFormSet):
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if any(self.errors):
+            return cleaned_data
+
+        if not self.data.get('check_hours'):
+            return cleaned_data
+
+        hours = []
+        assignment = self.instance
+
+        for form in self.forms:
+            if self.can_delete and self._should_delete_form(form):
+                continue
+            else:
+                hours.append(form.cleaned_data['hours'])
+
+        work_hours_max = Day.objects.get_work_hours_count(
+            assignment.start,
+            assignment.end,
+        )
+
+        if sum(hours) > work_hours_max:
+            message = _('%(hours_name)s must be less than or equal to'
+                        ' %(work_hours_max)s summary')
+            self._non_form_errors.append(
+                forms.ValidationError(
+                    message,
+                    code='invalid',
+                    params={
+                        'hours_name': self.model._meta.get_field(
+                            'hours',
+                        ).verbose_name,
+                        'assignment_name': self.model._meta.get_field(
+                            'assignment',
+                        ).verbose_name.lower(),
+                        'work_hours_max': work_hours_max,
+                    },
+                )
+            )
+        return cleaned_data
+
+
+ProjectAssignmentFormSet = forms.inlineformset_factory(
     Assignment,
     Assignment.projects.through,
-    form=ProjectAssignmentForm,
+    formset=BaseProjectAssignmentFormSet,
+    form=ProjectAssignmentFormsetForm,
     can_delete=True,
     extra=0,
 )
