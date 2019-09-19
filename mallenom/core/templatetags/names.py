25d57c6d49b056ecc0421cfc6d23ab377c3a2d0e
crowmurk <crowmurk@gmail.com>
2019-09-19 07:02:52 +0300

    refractor: Core templatetags

--- mallenom/core/templatetags/names.py
+++ mallenom/core/templatetags/names.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.exceptions import FieldDoesNotExist
 
 register = template.Library()
 
@@ -6,35 +7,43 @@ register = template.Library()
 def verbose_name(obj):
     """Фильтр получает verbose_name модели
     """
-    return getattr(obj._meta, 'verbose_name', '')
+    try:
+        return getattr(obj._meta, 'verbose_name', '')
+    except AttributeError:
+        return ''
 
 
 @register.filter
 def verbose_name_plural(obj):
     """Фильтр получает verbose_name_plural модели
     """
-    return getattr(obj._meta, 'verbose_name_plural', '')
+    try:
+        return getattr(obj._meta, 'verbose_name_plural', '')
+    except AttributeError:
+        return ''
 
 
 @register.simple_tag
 def field_verbose_name(obj, field):
     """Тег получает verbose_name поля заданой модели
     """
-    field = obj._meta.get_field(field)
+    try:
+        field = obj._meta.get_field(field)
+    except (FieldDoesNotExist, AttributeError):
+        return ''
 
     if hasattr(field, 'verbose_name'):
         # Обычное поле или RelationField forward
         return getattr(field, 'verbose_name', '')
-    try:
-        # RelationField reverce
-        related_model_meta = field.related_model._meta
-    except AttributeError:
-        return ''
-    return getattr(related_model_meta, 'verbose_name_plural', '')
+    # RelationField reverse
+    return getattr(field.related_model._meta, 'verbose_name_plural', '')
 
 
 @register.filter
 def form_field_class_name(obj):
     """Фильтр получает имя класса поля формы
     """
-    return obj.field.widget.__class__.__name__
+    try:
+        return obj.field.widget.__class__.__name__
+    except AttributeError:
+        return ''
