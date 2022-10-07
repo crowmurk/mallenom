335fa22f8a2d66c3c82f6bd9fa1c81a72142f0fa
crowmurk <crowmurk@gmail.com>
2022-10-07 10:54:53 +0300

    fix: Cyrillic symbols in url

--- mallenom/core/converters.py
+++ mallenom/core/converters.py
@@ -2,7 +2,7 @@ class RussianSlugConverter:
     """URL converter для поддержки кирилицы
     в адресной строке (необходима регистрация).
     """
-    regex = '[-а-яА-Яa-zA-Z0-9_]+'
+    regex = '[-а-яА-Яa-zA-Z0-9_ёЁ]+'
 
     def to_python(self, value):
         return value
